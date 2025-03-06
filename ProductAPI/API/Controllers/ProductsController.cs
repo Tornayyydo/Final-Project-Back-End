48dd80deeb660a55510a4545da4c80c8fa18d285
Tornayyydo <s_kurdghelashvili@cu.edu.ge>
2025-03-07 00:17:23 +0400

    new update

--- ProductAPI/API/Controllers/ProductsController.cs
+++ ProductAPI/API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductAPI.API.Dtos;
 using ProductAPI.API.Errors;
+using ProductAPI.API.Helpers;
 using ProductAPI.Core.Entities;
 using ProductAPI.Core.Interfaces;
 using ProductAPI.Core.Specifications;
@@ -31,13 +32,19 @@ namespace ProductAPI.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+            [FromQuery] ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductsWithFiltersForCountSpecification(productParams);
 
+            var totalItems = await _productsRepo.CountAsync(countSpec);
             var products = await _productsRepo.ListAsync(spec);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            var data = _mapper.Map<IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,
+                productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 using ProductAPI.API.Errors;
 using ProductAPI.API.Extensions;
@@ -8,9 +9,18 @@ using ProductAPI.API.MiddleWare;
 using ProductAPI.Core.Interfaces;
 using ProductAPI.Infrastructure;
 using ProductAPI.Infrastructure.Data;
+using StackExchange.Redis;
 using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy => policy.WithOrigins("http://localhost:4200") // ✅ Allow Angular frontend
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
 
 // Add services to the container.
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
@@ -20,6 +30,12 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<StoreDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<ConnectionMultiplexer>(c =>
+{
+    var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"));
+    return ConnectionMultiplexer.Connect(configuration);
+});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddSwaggerDocumentation();
@@ -45,6 +61,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseCors("AllowAngularApp");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -57,7 +75,12 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+    RequestPath = ""
+});
 
 app.UseAuthorization();
 
